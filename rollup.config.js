33c0aeae1a10b96f1ccd7ac9eaa475d5d201eee0
Goffert van Gool <ruphin@ruphin.net>
2018-06-17 05:45:18 +0200

    Update dependencies, clean up build pipeline

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import filesize from 'rollup-plugin-filesize';
 import uglify from 'rollup-plugin-uglify';
-import { uglifier } from 'uglify-es';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
@@ -18,11 +17,17 @@ function getConfig({ dest, format, uglified = true, transpiled = false }) {
       transpiled &&
         babel({
           presets: [['env', { modules: false }]],
-          plugins: ['transform-runtime'],
-          runtimeHelpers: true,
-          exclude: 'node_modules/**'
+          plugins: ['external-helpers']
         }),
-      uglified && uglify({ warnings: true, toplevel: !transpiled, sourceMap: true, compress: { passes: 2 }, mangle: { properties: false } }, uglifier),
+      uglified &&
+        uglify({
+          warnings: true,
+          toplevel: uglified,
+          sourceMap: uglified,
+          compress: uglified && { passes: 2 },
+          mangle: uglified,
+          output: { beautify: !uglified }
+        }),
       filesize()
     ].filter(Boolean)
   };
@@ -30,10 +35,6 @@ function getConfig({ dest, format, uglified = true, transpiled = false }) {
   return conf;
 }
 
-const config = [
-  getConfig({ dest: 'gluon-router.es5.js', format: 'iife', transpiled: true }),
-  getConfig({ dest: 'gluon-router.umd.js', format: 'umd' }),
-  getConfig({ dest: 'gluon-router.js', format: 'es' })
-];
+const config = [getConfig({ dest: 'gluon-router.es5.js', format: 'iife', transpiled: true }), getConfig({ dest: 'gluon-router.umd.js', format: 'umd' })];
 
 export default config;
